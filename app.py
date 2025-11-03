f1796e338ec82e51d9ad3f291b35384006de8271
adityale1711 <a.lesmana1711@gmail.com>
2025-11-03 14:15:03 +0700

    feat: initialize project and establish a clean architecture project structure

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+import sys
+from src.presentation.app import main
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+
+if __name__ == "__main__":
+    main()
---
+++ src/presentation/app.py
@@ -0,0 +1,7 @@
+def main() -> None:
+    """Entry point for the Streamlit application."""
+    print("Entry point for the Streamlit application.")
+
+
+if __name__ == "__main__":
+    main()
